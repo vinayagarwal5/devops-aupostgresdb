896386526c7a1b87b2d0c0493794dbb255e9d44d
vinay agrawal <vinayagarwal5@gmail.com>
2024-11-30 22:38:36 +0530

    add jenkins file

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "securepassword123"
+}
+
+variable "db_name" {
+  default = "auroradb"
+}
